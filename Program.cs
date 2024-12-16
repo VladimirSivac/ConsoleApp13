2141953759638dd82d6a70bb27a77dfeadd568d5
VladimirSivac <morndes@gmail.com>
2024-12-16 17:48:39 +0200

    task-3 done

--- Program.cs
+++ Program.cs
@@ -10,7 +10,30 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-           
+            int day = Input("Номер месяца: ");
+            string season = Season(day);
+            Console.WriteLine($"Время года: {season}");
+            Console.ReadLine();
         }
+        
+        static string Season(int day)
+        {
+            string season = "";
+            switch (day)
+            {
+                case 1: case 2: case 12: season = "Зима"; break;
+                case 3: case 4: case 5: season = "Весна"; break;
+                case 6: case 7: case 8: season = "Лето"; break;
+                case 9: case 10: case 11: season = "Осень"; break;
+            }
+            return season;
+        }
+        
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
     }
 }
