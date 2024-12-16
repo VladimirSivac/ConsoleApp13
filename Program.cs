e0764fb3a87560c8af40a21c23cab9ea516fb80a
VladimirSivac <morndes@gmail.com>
2024-12-16 17:50:02 +0200

    task-4 done

--- Program.cs
+++ Program.cs
@@ -10,7 +10,30 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-           
+            string firstLetter = Input("Первая буква предмета: ");
+            string subject = Subject(firstLetter);
+            Console.WriteLine($"Предмет: {subject}");
+            Console.ReadLine();
         }
+        
+        static string Subject(string firstLetter)
+        {
+            string subject = "";
+            switch (firstLetter.ToLower())
+            {
+                case "ф": subject = "Физика"; break;
+                case "м": subject = "Математика"; break;
+                case "и": subject = "История"; break;
+                case "г": subject = "География"; break;
+                case "б": subject = "Биология"; break;
+            }
+            return subject;
+        }
+        static string Input(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        
     }
 }
