28353574c544fa71a2f176c97d06afc85f54a4ab
VladimirSivac <morndes@gmail.com>
2024-12-16 17:52:24 +0200

    task-5 done

--- Program.cs
+++ Program.cs
@@ -10,7 +10,46 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-           
+            string firstLetter = Input("Первая буква транспорта: ");
+            string transport = Transport(firstLetter);
+            string maxSpeed = MaxSpeed(transport);
+            Console.WriteLine($"Вид транспорта: {transport}");
+            Console.WriteLine($"Максимальная скорость: {maxSpeed} км/ч!");
+            Console.ReadLine();
         }
+        static string MaxSpeed(string transport)
+        {
+            string maxSpeed = "";
+            switch (transport)
+            {
+                case "Автомобиль": maxSpeed = "60"; break;
+                case "Велосипед": maxSpeed = "15"; break;
+                case "Мотоцикл": maxSpeed = "120"; break;
+                case "Самолет": maxSpeed = "1000"; break;
+                case "Поезд": maxSpeed = "250"; break;
+            }
+            return maxSpeed;
+        }
+        
+        static string Transport(string firstLetter)
+        {
+            string transport = "";
+            switch (firstLetter.ToLower())
+            {
+                case "а": transport = "Автомобиль"; break;
+                case "в": transport = "Велосипед"; break;
+                case "м": transport = "Мотоцикл"; break;
+                case "с": transport = "Самолет"; break;
+                case "п": transport = "Поезд"; break;
+            }
+            return transport;
+        }
+        
+        static string Input(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        
     }
 }
